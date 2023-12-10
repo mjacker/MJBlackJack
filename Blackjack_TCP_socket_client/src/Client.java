1f66463edc2c5d4e4dd41b7c052b5272f9402d10
Jaime <mjackerjaime@gmail.com>
2023-12-10 20:27:59 +0800

    Cleaning code.

--- Blackjack_TCP_socket_client/src/Client.java
+++ Blackjack_TCP_socket_client/src/Client.java
@@ -4,15 +4,14 @@ import java.util.concurrent.*;
 import Objects.*;
 
 public class Client {
+
+	static String IP = "127.0.0.1";
+	static int PORT = 4999;
 	
 	public static void main(String[] args) throws IOException, InterruptedException{
 		
-		// Connect to the server on localhost (127.0.0.1) and port 4999
-		// Socket s = new Socket("140.118.245.205",4999);
-		// Socket s = new Socket("140.118.218.159",4999);
-
-
 		Card card = Receivecard();
+
 		System.out.println("The receive card is: " + card.toString());
 		card = Receivecard();
 		System.out.println("The receive card is: " + card.toString());
@@ -26,13 +25,11 @@ public class Client {
 		SendCard(c1);
 		SendCard(c2);
 		SendCard(c3);
-
-
 	}
 		
 	public static Card SendCard(Card scard){
 		try {
-			Socket socket = new Socket("127.0.0.1",4999);
+			Socket socket = new Socket(IP, PORT);
 		
 			// Send the message to the server
 			PrintWriter pr = new PrintWriter(socket.getOutputStream());
@@ -46,49 +43,15 @@ public class Client {
 			// Close the ServerSocket
 			socket.close();
 		
-			}catch(Exception e) {
-				// Close the ServerSocket
-				// serversocket.close();
-			}
-		
-				return scard;
+		}catch(Exception e) {
+			// Close the ServerSocket
+			// serversocket.close();
+		}
+		return scard;
 	}
 
-/* 
-		// Prepare a message to send to the server
-		//String message2send = "Hello";
-		//int message2send = 7;
-		String stringcard = card.toString();
-		
-		// Send the message to the server
-		PrintWriter pr = new PrintWriter(s.getOutputStream());
-		// pr.println(stringcard);
-		pr.println(stringcard);
-		pr.flush();
-		
-		// System.out.println(messagefromclient);
-		
-		// Set up input stream to read data from the server
-		InputStreamReader in = new InputStreamReader(s.getInputStream());
-		BufferedReader bf = new BufferedReader(in);
-		
-		// Read a message from the server
-		String str = bf.readLine();
-		System.out.println(str);
-		
-		// Pause for 1 second
-		System.out.println("------------------------------");
-		TimeUnit.SECONDS.sleep(1);
-		
-		// Close the socket
-		s.close();
-		*/
-
-		public static Card Receivecard(){
-			Card rcard = null;
-		// Create a ServerSocket that listens on port 4999
-
-		// System.out.println("Wait for connection");
+	public static Card Receivecard(){
+		Card rcard = null;
 		try {
 			Socket socket = new Socket("127.0.0.1",4999);
 		
@@ -97,19 +60,7 @@ public class Client {
 			BufferedReader bf = new BufferedReader(in);
 			// Read a message from the client
 			rcard = new Card(bf.readLine());
-			// System.out.println(receivecard.toString());
-		
-			// Prepare a message to send to the client
-			// String messagefromserver = "chau";
-			// System.out.println(messagefromserver);
-		
-			// Set up output stream to send data to the client
-			// PrintWriter pr = new PrintWriter(socket.getOutputStream());
-			// pr.println(messagefromserver);
-			// pr.flush();
 
-			// Pause for 1 second
-			// System.out.println("------------------------------");
 			TimeUnit.SECONDS.sleep(1);
 				
 			// Close the ServerSocket
@@ -117,9 +68,7 @@ public class Client {
 		}catch(Exception e) {
 			// Close the ServerSocket
 			// serversocket.close();
-		
 		}
-
 		return rcard;
 	}
 }
--- Blackjack_TCP_socket_server/src/Server.java
+++ Blackjack_TCP_socket_server/src/Server.java
@@ -19,9 +19,6 @@ public class Server {
 		System.out.println(c1);
 		System.out.println(c2);
 		System.out.println(c3);
-		// FIRST CHECKPOINT
-		// card = Receivecard(serversocket);
-		// System.out.println("la carta que recivo es: " + card.toString());
 		
 		//SECONDS CHECKPOINT
 		SendCard(serversocket,c1);
@@ -33,58 +30,6 @@ public class Server {
 		System.out.println(Receivecard(serversocket).toString());
 
 		serversocket.close();
-
-
-		/* 
-		System.out.println("Wait for connection");
-		try {
-			// Accept a client connection
-			Socket socket = serversocket.accept();
-			   
-			System.out.println("Client connected");
-			System.out.println("##########################################");
-			System.out.println("Port: " + serversocket.getLocalPort());
-			System.out.println("Receive Buffer size: " + serversocket.getReceiveBufferSize());
-			System.out.println("Reuse Address: " + serversocket.getReuseAddress());
-			System.out.println("Get Channel: " + serversocket.getChannel());
-			System.out.println("Class: " + serversocket.getClass());
-			System.out.println("Inet Address: " + serversocket.getInetAddress());
-			System.out.println("Local Socket: " + serversocket.getLocalSocketAddress());
-			System.out.println("##########################################");
-		
-			// Loop to exchange messages with the client 3 times
-			for (int i=1; i<=2; i++ ) {
-							// Set up input stream to read data from the client
-				InputStreamReader in = new InputStreamReader(socket.getInputStream());
-				BufferedReader bf = new BufferedReader(in);
-
-				// Read a message from the client
-				String str = bf.readLine();
-				System.out.println(str);
-			
-				// Prepare a message to send to the client
-				String messagefromserver = "Server send " + i + ":" + "World!";
-			
-				System.out.println(messagefromserver);
-			
-				// Set up output stream to send data to the client
-				PrintWriter pr = new PrintWriter(socket.getOutputStream());
-				pr.println(messagefromserver);
-				pr.flush();
-
-				// Pause for 1 second
-				System.out.println("------------------------------");
-				TimeUnit.SECONDS.sleep(1);
-				
-			}
-			// Close the ServerSocket
-			socket.close();
-		}catch(Exception e) {
-			// Close the ServerSocket
-			serversocket.close();
-		
-		}
-		*/
 	}
 	public static Card SendCard(ServerSocket serversocket, Card scard){
 		// Create a ServerSocket that listens on port 4999
@@ -108,50 +53,33 @@ public class Server {
 
 		}catch(Exception e) {
 			// Close the ServerSocket
-			// serversocket.close();
+			// socket.close();
 		}
 
 		return scard;
 	}
 
 	public static Card Receivecard(ServerSocket serversocket){
-		// Create a ServerSocket that listens on port 4999
 		System.out.println("Wait for connection");
 		Card rcard = null;
 		try {
 			// Accept a client connection
 			Socket socket = serversocket.accept();
-			   
 		
 			// Set up input stream to read data from the client
 			InputStreamReader in = new InputStreamReader(socket.getInputStream());
 			BufferedReader bf = new BufferedReader(in);
 			// Read a message from the client
 			rcard = new Card(bf.readLine());
-			// System.out.println(receivecard.toString());
-		
-			// Prepare a message to send to the client
-			// String messagefromserver = "chau";
-			// System.out.println(messagefromserver);
-		
-			// Set up output stream to send data to the client
-			// PrintWriter pr = new PrintWriter(socket.getOutputStream());
-			// pr.println(messagefromserver);
-			// pr.flush();
 
-			// Pause for 1 second
-			// System.out.println("------------------------------");
 			TimeUnit.SECONDS.sleep(1);
 				
 			// Close the ServerSocket
 			socket.close();
 		}catch(Exception e) {
 			// Close the ServerSocket
-			// serversocket.close();
-		
+			// socket.close();
 		}
-
 		return rcard;
 	}
-
 }
